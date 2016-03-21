9161c7a73c17914fefd98e2969872f665b6f28c8
loveprolife <815117718@qq.com>
2016-03-21 14:04:39 +0800

    Update 2.php结构型模式.php

--- 2.php结构型模式.php
+++ 2.php结构型模式.php
@@ -2,7 +2,47 @@
 
 ![image](https://github.com/loveprolife/IMG/blob/master/shipeiqi.gif)
 
+<?php
 
+interface Target {
+	public function sampleMethod1 ();
+	public function sampleMethod2 ();
+}
+
+class Adaptee {
+	public function sampleMethod1 () {
+		echo 'Adaper sampleMethod1<br/>';
+	}
+}
+
+class Apapter implements Target {
+	private $_adaptee = NULL;
+
+	public function __construct (Adaptee $adaptee) {
+		$this->_adaptee = $adaptee;
+	}
+
+	public function sampleMethod1 () {
+		$this->_adaptee->sampleMethod1();
+	}
+
+	public function sampleMethod2 () {
+		echo 'Adaper sampleMethod2<br/>';
+	}
+}
+
+class Client {
+	public static function main () {
+		$adaptee = new Adaptee();
+		
+		$adapter = new Apapter($adaptee);
+		$adapter->sampleMethod1();
+		$adapter->sampleMethod2();
+	}
+}
+
+Client::main();
+?>
 
 
 
