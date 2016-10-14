17423dc22724a06231c9aa87d493fcc2cf932747
loveprolife <815117718@qq.com>
2016-10-14 13:47:09 +0800

    Update 2.php结构型模式.php

--- 2.php结构型模式.php
+++ 2.php结构型模式.php
@@ -331,6 +331,78 @@ Client::main();
 
 ![image](https://github.com/loveprolife/IMG/blob/master/menmiaomoshi_waiguanmoshi.png)
 
+<?php
+
+/**阿里股票
+ * Class Ali
+ */
+class Ali{
+    function buy(){
+        echo "买入阿里股票<br/>";
+    }
+
+    function sell(){
+        echo "卖出阿里股票<br/>";
+    }
+}
+
+/**万达股票
+ * Class Wanda
+ */
+class Wanda{
+    function buy(){
+        echo "买入万达股票<br/>";
+    }
+
+    function sell(){
+        echo "卖出万达股票<br/>";
+    }
+}
+
+/**京东股票
+ * Class Jingdong
+ */
+class Jingdong{
+    function buy(){
+        echo "买入京东股票<br/>";
+    }
+
+    function sell(){
+        echo "卖出京东股票<br/>";
+    }
+}
+
+/**门面模式核心角色
+ * Class FacadeCompany
+ */
+class FacadeCompany{
+    private $ali;
+
+    private $wanda;
+
+    private $jingdong;
+
+    function __construct(){
+        $this->ali=new Ali();
+        $this->jingdong=new Jingdong();
+        $this->wanda=new Wanda();
+    }
+
+    function buy(){
+        $this->wanda->buy();
+        $this->ali->buy();
+    }
+
+    function sell(){
+        $this->jingdong->sell();
+    }
+}
+
+$lurenA = new FacadeCompany();
+$lurenA->buy();
+$lurenA->sell();
+
+?>
 
 
 6.代理模式
