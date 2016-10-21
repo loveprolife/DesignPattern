2669289cbe4c6986edec44c9804601323690ad03
loveprolife <815117718@qq.com>
2016-10-21 10:02:28 +0800

    Update 3.php行为型模式.php

--- 3.php行为型模式.php
+++ 3.php行为型模式.php
@@ -275,6 +275,63 @@ $concreteSubject->condition(1000);
 
 4.迭代器模式
 
+![image](https://github.com/loveprolife/IMG/blob/master/observer.jpg)
+
+<?php
+
+//抽象迭代器
+abstract class IIterator{
+    public abstract function First();
+    public abstract function Next();
+    public abstract function IsDone();
+    public abstract function CurrentItem();
+}
+
+//具体迭代器
+class ConcreteIterator extends IIterator{
+
+    private $aggre;
+    private $current = 0;
+
+    public function __construct(array $_aggre){
+        $this->aggre = $_aggre;
+    }
+
+    //返回第一个
+    public function First(){
+        return $this->aggre[0];
+    }
+
+    //返回下一个
+    public function  Next(){
+        $this->current++;
+        if($this->current<count($this->aggre)) {
+            return $this->aggre[$this->current];
+        }
+        return false;
+    }
+
+    //返回是否IsDone
+    public function IsDone(){
+        return ($this->current >= count($this->aggre)) ? true : false;
+    }
+
+    //返回当前聚集对象
+    public function CurrentItem(){
+        return $this->aggre[$this->current];
+    }
+}
+
+$iterator= new ConcreteIterator(array('周杰伦','王菲','周润发'));
+$item = $iterator->First();
+echo $item."<br/>";
+while(!$iterator->IsDone()){
+    echo "{$iterator->CurrentItem()}：请买票！<br/>";
+    $iterator->Next();
+}
+
+?>
+
 5.责任链模式
 
 6.命令模式
